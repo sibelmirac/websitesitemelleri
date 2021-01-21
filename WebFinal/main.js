6575cff7e758ce6f27b627af56f6069949daee31
sibelmirac <sibelmiracozcann@gmail.com>
2021-01-22 00:10:18 +0300

    Final Projesi/COVID-19

---
+++ WebFinal/main.js
@@ -0,0 +1,10 @@
+(function ($) {
+    ("use strict");
+
+    $(".menu-toggle").click(function(e){
+        e.preventDefault();
+        $("#sidebar-wrapper").toggleClass("active");
+        $(".menu-toggle > .fa-bars, .menu-toggle > .fa-times").toggleClass("fa-bars fa-times");
+        $(this).toggleClass("active");
+    });
+})(jQuery);
